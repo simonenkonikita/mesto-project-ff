5a9308e36f75fec59c9a8b82adcc7066214aa308
Nikita Simonenko <simonenkonikita27@yandex.ru>
2024-12-06 21:03:41 +0300

    1 commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const templateContainer = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
 
-// @todo: Функция создания карточки
+initialCards.forEach(function (item) {
+  const cardElement = templateContainer.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
+  cardContainer.append(cardElement);
+});
 
-// @todo: Функция удаления карточки
+function addCard() {
+  const cardItem = templateContainer.querySelector('.places__item').cloneNode(true);
+  cardContainer.append(cardItem);
+} 
 
-// @todo: Вывести карточки на страницу
+addButton.addEventListener('click', addCard);
+
+const cardElement = templateContainer.querySelector('.places__item');
+const deleteButton = document.querySelector('card__delete-button');
+
+
+function deleteBtn() {
+  cardContainer.remove();
+ 
+};
+
+deleteButton.addEventListener('click', deleteBtn);
+
+
